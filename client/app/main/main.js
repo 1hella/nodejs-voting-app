1454f84a61803310880bfd6bc1c239dfab8ee4f2
Stephen Wanhella <stephen.wanhella@gmail.com>
2015-07-30 21:27:03 +0000

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
