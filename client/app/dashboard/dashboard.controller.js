1a809aab9ebe4cf952a7e1c2a9a541cfe3d8d611
Stephen Wanhella <stephen.wanhella@gmail.com>
2015-08-18 20:01:01 -0700

    mongoose poll schema; poll submit dashboard page

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,15 +1,35 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('DashboardCtrl', function($scope) {
+  .controller('DashboardCtrl', function($scope, $http, Auth) {
     $scope.tab = 1; // new poll tab
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.myPolls = [];    
     $scope.poll = {
+      author: $scope.getCurrentUser().name,
       name: '',
       options: [{
-        name: ""
+        name: ''
       }, {
-        name: ""
+        name: ''
       }]
+    };
+
+    $http.get('/api/polls').success(function(myPolls) {
+      $scope.myPolls = myPolls;
+    });
+    
+    /* New Poll */
+    var clearPoll = function() {
+      $scope.poll = {
+        author: $scope.getCurrentUser().name,
+        name: '',
+        options: [{
+          name: ''
+        }, {
+          name: ''
+        }]
+      };
     };
 
     $scope.setTab = function(tab) {
@@ -38,4 +58,19 @@ angular.module('workspaceApp')
     $scope.deleteOption = function(index) {
       $scope.poll.options.splice(index, 1);
     };
+
+    $scope.submitPoll = function() {
+      $http.post('/api/polls', $scope.poll).
+      then(function(response) {
+        $scope.myPolls.push(response.data);
+        console.dir($scope.myPolls);
+        clearPoll();
+        console.dir('Successfully inserted poll');
+        $scope.setTab(2);
+      }, function error(err) {
+        console.dir('error', err);
+      });
+    };
+
+    /* My Polls */
   });
