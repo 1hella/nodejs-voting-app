00952588d59d8b98029460af7f900284243bcf49
swanhell <swanhell@sfu.ca>
2018-04-19 16:34:50 -0700

    fix crypto error

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -142,7 +142,7 @@ UserSchema.methods = {
   encryptPassword: function(password) {
     if (!password || !this.salt) return '';
     var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+    return crypto.pbkdf2Sync(password, salt, 10000, 64, null).toString('base64');
   }
 };
 
