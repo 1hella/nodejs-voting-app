bb42375d24ae919af88dbe70eb501038035d84f0
swanhell <stephen_wanhella@sfu.ca>
2019-02-20 14:34:18 -0800

    remove connect-livereload

--- server/config/express.js
+++ server/config/express.js
@@ -52,7 +52,6 @@ module.exports = function(app) {
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', path.join(config.root, 'client'));
