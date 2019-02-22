26c39f43a1fa894739692c1e6443cc94449c3c32
swanhell <swanhell@sfu.ca>
2019-02-21 17:58:33 -0800

    fix https function

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -11,8 +11,7 @@ function requiredProcessEnv(name) {
 }
 
 function makeHttps(domain) {
-  console.log(domain ? domain.replace(/^http:\/\//i, 'https://') : domain);
-  return domain ? domain.replace(/^http:\/\//i, 'https://') : domain;
+  return domain ? domain.replace(/^http:\/\//i, 'https://') : '';
 }
 
 // All configurations will extend these options
