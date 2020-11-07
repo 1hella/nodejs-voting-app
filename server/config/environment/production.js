65231a53ab542379524158c6427805c75168e051
swanhell <stephen_wanhella@sfu.ca>
2020-11-06 18:29:46 -0800

    remove mongolab

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,8 +15,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
+    uri:    process.env.MONGOHQ_URL ||
             process.env.MONGODB_URI ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/workspace'
